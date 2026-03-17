605a3f5f9df1fd9f41a432fdb88810f88d1c945b
shariarSultanFahim <shariarsfahim@gmail.com>
2026-03-17 23:14:41 +0600

    fix: add spacing to FormMessage component in AddBikeDialog

--- src/app/(private)/user/profile/components/dialog/add-bike.dialog.tsx
+++ src/app/(private)/user/profile/components/dialog/add-bike.dialog.tsx
@@ -163,7 +163,7 @@ export function AddBikeDialog({
                         {...field}
                       />
                     </FormControl>
-                    <FormMessage />
+                    <FormMessage /> 
                   </FormItem>
                 )}
               />
