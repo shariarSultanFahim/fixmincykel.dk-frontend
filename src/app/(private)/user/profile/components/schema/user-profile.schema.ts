703e63e595b930bce67648d7be94d4fa16abc86b
shariarSultanFahim <shariarsfahim@gmail.com>
2026-03-27 01:49:56 +0600

    fix: update user profile schema validation for phone and address fields

--- src/app/(private)/user/profile/components/schema/user-profile.schema.ts
+++ src/app/(private)/user/profile/components/schema/user-profile.schema.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const userProfileSchema = z.object({
   fullName: z.string().min(2, "Full name is required"),
-  email: z.string().email("Enter a valid email"),
-  phone: z.string().min(6, "Enter a valid phone number"),
-  address: z.string().min(6, "Enter a valid address")
+  email: z.email("Enter a valid email"),
+  phone: z.string().min(1, "Enter a valid phone number"),
+  address: z.string().min(1, "Enter a valid address")
 });
